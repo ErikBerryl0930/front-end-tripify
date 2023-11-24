f57f56ad9bcb1d70152469c7e5f40377fc29236a
nisfu-saaban <nisthy07@gmail.com>
2023-11-24 15:29:51 +0800

    home

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
@@ -7,153 +8,178 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> dest = [
-      'padimas',
-      'bambang',
-      'makaroni',
-      'hewes',
-      'konst',
-      'naspad',
-      'nasgor',
-      'item',
-      'tembem',
-      'berbuli',
-      'apatuch',
-      'kok becek',
-    ];
-
-    final HomeController controller = Get.put(HomeController());
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        padding: EdgeInsets.all(16),
+      body: Padding(
+        padding: EdgeInsets.only(left: 22, right: 22),
         child: Column(
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  child: Text(
-                    'Hi, Username',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
+                Text(
+                  "Hi, John Doe!",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
                 Container(
-                  child: CircleAvatar(),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.4),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(
+                        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"),
+                  ),
                 ),
               ],
             ),
-            Container(
-              child: TextField(
-                controller: controller.searchController,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.search),
-                  labelText: 'Search here',
-                ),
-              ),
+            SizedBox(
+              height: 30,
             ),
             Container(
-              alignment: AlignmentDirectional.centerStart,
-              child: Column(
-                children: [
-                  Text(
-                    "Category",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w800,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade400,
+                      blurRadius: 15,
+                      offset: const Offset(0, 10),
                     ),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    prefixIcon: Icon(Icons.search),
+                    hintText: "Search destination",
+                    fillColor: Colors.white,
+                    filled: true,
                   ),
-                ],
-              ),
+                )),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Category",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                )
+              ],
+            )),
+            SizedBox(
+              height: 10,
             ),
             Container(
               height: 100,
-              width: MediaQuery.of(context).size.width,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                controller: ScrollController(),
-                child: Row(
-                  children: List.generate(
-                    10,
-                    (index) {
-                      var item = {};
-                      bool selected = index == 0;
-
-                      return Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 12.0,
-                          vertical: 8.0,
-                        ),
-                        margin: const EdgeInsets.only(right: 10.0),
-                        decoration: BoxDecoration(
-                          color: selected ? Colors.amber : Colors.grey,
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(16.0),
-                          ),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Menu ${index + 1}",
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 11.0,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
               child: ListView.builder(
-                itemCount: (dest.length / 2).ceil(),
-                physics: const ScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: 20,
                 itemBuilder: (BuildContext context, int index) {
-                  int startIndex = index * 2;
-                  int endIndex = (index + 1) * 2;
-                  endIndex > dest.length ? dest.length : endIndex;
-                  return Row(
-                      children: List.generate(endIndex - startIndex, (indexOf) {
-                    int current = startIndex + indexOf;
-                    return Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: desCard(dest[current]),
-                      ),
-                    );
-                  }));
+                  return Container(
+                    width: 80,
+                    height: 30,
+                    margin: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundImage: NetworkImage(
+                              "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text("person ${index}")
+                      ],
+                    ),
+                  );
                 },
               ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget desCard(String dest) {
-    return Card(
-      elevation: 3,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              dest,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              child: Row(
+                children: [
+                  Text(
+                    "Recomendation",
+                    style: TextStyle(fontSize: 20),
+                  )
+                ],
               ),
             ),
-            SizedBox(height: 8),
-            // Add other details or widgets related to the fruit
+            SizedBox(
+              height: 15,
+            ),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: (12 / 2).ceil(),
+                    itemBuilder: (context, index) {
+                      int start = index * 2;
+                      int end = (index + 1) * 2;
+                      end = end > 12 ? 12 : end;
+                      return Row(
+                        children: List.generate(end - start, (sub) {
+                          int current = start + sub;
+                          return Expanded(
+                              child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 100,
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                            "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"),
+                                        fit: BoxFit.cover),
+                                  ),
+                                ),
+                                Text("person ${current}")
+                              ],
+                            ),
+                          ));
+                        }),
+                      );
+                    })),
           ],
         ),
       ),
+      bottomNavigationBar:
+          BottomNavigationBar(type: BottomNavigationBarType.fixed, items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: 'favorite',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ]),
     );
   }
 }
