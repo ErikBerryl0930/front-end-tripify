25ba5ecf1fa26810fd6dde395ccf5d93ffe15217
christoforadiana <christoforadianayuliawati@gmail.com>
2023-12-05 06:14:36 +0700

    transaction error parsing

--- lib/views/home/section_transaction.dart
+++ lib/views/home/section_transaction.dart
@@ -39,7 +39,6 @@ class _SectionTransactionState extends State<SectionTransaction> {
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: const Color(0xFF004C5C).withOpacity(0.5),
         title: Text(
           'Transaction',
           style: GoogleFonts.poppins(
@@ -56,7 +55,7 @@ class _SectionTransactionState extends State<SectionTransaction> {
             // padding: const EdgeInsets.symmetric(horizontal: 30),
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            itemCount: transactions.isNotEmpty ? transactions.length : 1,
+            itemCount: transactions.length,
             itemBuilder: (context, index) {
               if (transactions.isEmpty) {
                 return Column(
@@ -74,93 +73,51 @@ class _SectionTransactionState extends State<SectionTransaction> {
                   ],
                 );
               } else {
-                return Card(
-                  child: Container(
-                    height: 200,
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    child: Row(
-                      children: [
-                        // Container(
-                        //   margin: const EdgeInsets.only(right: 12),
-                        //   height: 100,
-                        //   width: 100,
-                        //   decoration: BoxDecoration(
-                        //     borderRadius: BorderRadius.circular(12),
-                        //     image: const DecorationImage(
-                        //       image: NetworkImage(
-                        //           'https://images.unsplash.com/photo-1578469550956-0e16b69c6a3d?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
-                        //       fit: BoxFit.cover,
-                        //     ),
-                        //   ),
-                        // ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Row(
-                                // crossAxisAlignment: CrossAxisAlignment.end,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(transactions[index].invoiceNumber,
-                                      style: GoogleFonts.poppins(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.black)),
-                                  const Row(
-                                    children: [
-                                      Icon(
-                                        Icons.paid,
-                                        color: Colors.yellow,
-                                        size: 25,
-                                      ),
-                                      // Text('transaction.status.toString()'),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              const Row(
-                                children: [
-                                  Icon(
-                                    Icons.location_pin,
-                                    size: 12,
-                                  ),
-                                  Text('transaction.location'),
-                                ],
-                              ),
-                              const Row(
-                                children: [
-                                  Icon(
-                                    Icons.person,
-                                    size: 12,
-                                  ),
-                                  Text('transaction.person'),
-                                ],
-                              ),
-                              const Row(
-                                children: [
-                                  Icon(
-                                    Icons.money,
-                                    size: 12,
-                                  ),
-                                  Text('transaction.description'),
-                                ],
-                              ),
-                              const Row(
-                                children: [
-                                  Icon(
-                                    Icons.calendar_today,
-                                    size: 12,
-                                  ),
-                                  Text('transaction.date'),
-                                ],
-                              ),
-                            ],
+                return Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ListView.builder(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    itemCount: transactions.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          // _navigateToDetails(destinations[index].id);
+                        },
+                        child: Card(
+                          elevation: 5,
+                          margin: const EdgeInsets.all(5.0),
+                          child: ListTile(
+                            title: Text(
+                              transactions[index].invoiceNumber,
+                              style: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Row(
+                                //   children: [
+                                //     const Icon(
+                                //       Icons.location_on,
+                                //       color: Colors.grey,
+                                //     ),
+                                //     Text(filteredDestinations[index].region,
+                                //         style: const TextStyle(
+                                //             fontWeight: FontWeight.w400,
+                                //             color: Colors.grey)),
+                                //   ],
+                                // ),
+                                // Text(filteredDestinations[index].description,
+                                //     overflow: TextOverflow.ellipsis,
+                                //     style: const TextStyle(
+                                //         fontWeight: FontWeight.w400,
+                                //         color: Colors.black)),
+                              ],
+                            ),
                           ),
                         ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
                 );
               }
